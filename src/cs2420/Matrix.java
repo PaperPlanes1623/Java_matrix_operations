d7a0e49121cf76e35e1b16f399a53e8e8f98c3a8
Dylan Taft <dylbronjames@gmail.com>
2019-08-27 23:34:30 -0600

    init repo

---
+++ src/cs2420/Matrix.java
@@ -0,0 +1,117 @@
+package cs2420;
+
+/**
+ * Represents a 2-d integer matrix as a 2-d int array.
+ * 
+ * @author Swaroop Joshi
+ *
+ */
+public class Matrix {
+	/**
+	 * Holds the number of rows of this matrix
+	 */
+	private int numRows;
+	/**
+	 * Holds the number of columns of this matrix
+	 */
+	private int numColumns;
+	/**
+	 * Holds the actual data of this matrix
+	 */
+	private int data[][];
+
+	/**
+	 * Constructor from a 2D array -- automatically determines the dimensions.
+	 * 
+	 * @param d 2D array to construct the matrix
+	 * @throws IllegalArgumentException if {@code d} is empty or null
+	 */
+	public Matrix(int d[][]) throws IllegalArgumentException {
+		if (d == null || d.length == 0) {
+			throw new IllegalArgumentException();
+		}
+		this.numRows = d.length; // d.length is the number of 1D arrays in the 2D array
+		this.numColumns = d[0].length; // d[0] is the first 1D array
+
+		// create a new matrix to hold the data
+		this.data = new int[this.numRows][this.numColumns];
+		// copy the data over
+		for (int i = 0; i < this.numRows; i++) {
+			for (int j = 0; j < this.numColumns; j++) {
+				this.data[i][j] = d[i][j];
+			}
+		}
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o == null) {
+			return false; // Why?
+		}
+		if (!(o instanceof Matrix)) { // make sure the Object we're comparing to is a Matrix
+			return false;
+		}
+		Matrix m = (Matrix) o; // if the above was not true, we know it's safe to treat 'o' as a Matrix
+
+		/*
+		 * TODO: replace the below return statement with the correct code, you must
+		 * return the correct value after determining if this matrix is equal to the
+		 * input matrix
+		 */
+		return false; // placeholder
+	}
+
+	@Override
+	public String toString() {
+		
+		String output = "";
+		
+		for(int i=0; i<numRows; i++) {
+			for(int j=0; j<numColumns; j++) {
+				output += data[i][j] + " ";
+			}
+			output +="\n";
+		}
+				
+		
+		
+		return output; // placeholder; don't use explicit nulls otherwise
+	}
+
+	/**
+	 * Multiplies {@code this} by {@code m} and returns the result as a new
+	 * {@code Matrix}. If {@code m} does not have appropriate dimensions to multiply
+	 * to {@code this}, it throws an {@code IllegalArgumentException}.
+	 * 
+	 * @param m matrix to multiply {@code this} with
+	 * @return result of the product
+	 * 
+	 * @throws IllegalArgumentException if dimensions of the matrices are not
+	 *                                  compatible
+	 */
+	public Matrix times(Matrix m) throws IllegalArgumentException {
+		/*
+		 * TODO: implement the method to match the description
+		 */
+		return null; // placeholder; don't use explicit nulls otherwise
+	}
+
+	/**
+	 * Adds {@code m} to {@code this} and returns the result as a new
+	 * {@code Matrix}. If {@code m} does not have the same dimensions as
+	 * {@code this}, it throws an {@code IllegalArgumentException}.
+	 * 
+	 * @param m matrix to add to {@code this} with
+	 * @return result of the addition
+	 *
+	 * @throws IllegalArgumentException if dimensions of the matrices are not
+	 *                                  compatible
+	 */
+	public Matrix plus(Matrix m) throws IllegalArgumentException {
+		
+		if(m == null) {
+			throw new IllegalArgumentException("m cannot be null");
+		}
+		return null; // placeholder; don't use explicit nulls otherwise
+	}
+}
---
+++ src/cs2420/MatrixTester.java
@@ -0,0 +1,75 @@
+package cs2420;
+
+import components.simplewriter.SimpleWriter;
+import components.simplewriter.SimpleWriter1L;
+
+/**
+ * Skeleton to test your matrix implementation. Some test cases are provided.
+ * You need to add many more.
+ * 
+ * @author Swaroop Joshi
+ *
+ */
+public class MatrixTester {
+	public static void main(String[] args) {
+		SimpleWriter out = new SimpleWriter1L();
+
+		/*
+		 * Test case 1: constructing a matrix from an empty array should throw an
+		 * IllegalArgumentException.
+		 */
+		int[][] d = new int[0][0];
+		boolean caughtRightException = false;
+		Matrix m;
+		try {
+			m = new Matrix(d);
+		} catch (IllegalArgumentException e) {
+			out.println("Success: constructing a matrix from an empty array");
+			caughtRightException = true;
+		}
+		if (!caughtRightException) {
+			out.print("Fail: constructing a matrix from an empty array");
+			caughtRightException = false; // reset for next such test case
+		}
+
+		/*
+		 * Test case 2: a valid matrix multiplication
+		 */
+		Matrix m1 = new Matrix(new int[][] { { 1, 2, 3 }, { 2, 5, 6 } });
+		/*
+		 * Testing toString
+		 */
+		String m1StrExp = "1 2 3 \n2 5 6 \n";
+		String m1StrActual = m1.toString();
+		if (!m1StrExp.equals(m1StrActual)) {
+			out.println("toString Error.\nGot:\n" + m1StrActual + "\nExpected:\n" + m1StrExp);
+		}
+
+		/*
+		 * Testing equals
+		 */
+		Matrix m1Expected = new Matrix(new int[][] { { 1, 2, 3 }, { 2, 5, 6 } });
+		if (!m1Expected.equals(m1)) {
+			out.println("Equals error: m1 and m1Expected not equal\nGot:" + m1 + "\nexpected:" + m1Expected + ")");
+		}
+
+		/*
+		 * Testing times (!! Pun unintended !!)
+		 */
+		Matrix m2 = new Matrix(new int[][] { { 4, 5 }, { 3, 2 }, { 1, 1 } });
+		Matrix actualProduct = m1.times(m2);
+		Matrix expectedProduct = new Matrix(new int[][] { { 13, 12 }, { 29, 26 } }); // we know this from math
+		if (!expectedProduct.equals(actualProduct)) {
+			out.println("equals error (m1 * m2 not equal to expected result)");
+		}
+
+		/*
+		 * TODO: fill in more tests that fully exercise all of your methods
+		 */
+
+		/*
+		 * Finally close the output stream
+		 */
+		out.close();
+	}
+}
